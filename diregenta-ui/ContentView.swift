fbd23968e7786a5b52ba154d20c12f60fdb6f9a0
Bernhard Häussner <bxt@die-optimisten.net>
2026-02-18 17:09:29 +0100

    feat: initialize project

---
+++ diregenta-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  diregenta-ui
+//
+//  Created by Bernhard Häussner on 18.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ diregenta-ui/diregenta_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  diregenta_uiApp.swift
+//  diregenta-ui
+//
+//  Created by Bernhard Häussner on 18.02.26.
+//
+
+import SwiftUI
+
+@main
+struct diregenta_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
